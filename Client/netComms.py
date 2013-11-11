e1e00d8d9775840b1a17f33e697693352a2cddae
Harrry Ward <Harry.ward9001@gmail.com>
2013-11-11 17:09:30 +0000

    Restructure and conf added

--- Client/netComms.py
+++ Client/netComms.py
@@ -1,19 +1,10 @@
 import socket
 import pickle
-import errno
-
-class networkComms():
-    def __init__(self,  ip , newport_gwent):
-        self.ip = ip
-        self.port = newport_gwent
-        self.sock = socket.create_connection((self.ip,  self.port))
+class networkComms(ip,  port):
     def send(self, message):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((ip,  port))
         self.toSend = pickle.dumps(message)
-        try:
-            self.sock.sendall(self.toSend)
-            self.recieved = pickle.loads(self.sock.recv(2048))
-        except Exception:
-            del self.sock
-            self.sock = socket.create_connection((self.ip,  self.port))
-    def close(self):
+        self.sock.sendall(self.toSend)
+        self.recieved = pickle.loads(self.sock.recv(1024))
         self.sock.close()
